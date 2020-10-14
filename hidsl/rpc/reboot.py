4fa32d5b63c6376076b22de02e2dd039aed5fb12
Richard Neumann <r.neumann@homeinfo.de>
2020-10-14 13:17:34 +0200

    Refac.

--- hidsl/rpc/reboot.py
+++ hidsl/rpc/reboot.py
@@ -16,15 +16,16 @@ def reboot(system: int, args: Namespace, job: NamespaceProxy) -> bool:
     """Reboots a system."""
 
     command = ssh(system, *sudo(SYSTEMCTL, 'reboot'), no_stdin=args.no_stdin)
+    LOGGER.debug('Rebooting system %i.', system)
     completed_process = execute(command)
     job['reboot'] = completed_process.returncode
 
     if completed_process.returncode == 0:
-        LOGGER.info('System %i is rebooting.')
+        LOGGER.info('System %i is rebooting.', system)
         return True
 
     if completed_process.returncode == 1:
-        LOGGER.warning('System %i may be rebooting.')
+        LOGGER.warning('System %i may be rebooting.', system)
         return True
 
     if completed_process.returncode == 255:
