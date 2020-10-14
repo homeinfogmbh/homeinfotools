91a513739eead82d8f810d3317926a6f1211645e
Richard Neumann <r.neumann@homeinfo.de>
2020-10-14 13:23:35 +0200

    Refac.

--- hidsl/rpc/runcmd.py
+++ hidsl/rpc/runcmd.py
@@ -16,19 +16,26 @@ def runcmd(system: int, args: Namespace, job: DictProxy) -> bool:
 
     result = True
 
-    for index, command in enumerate(args.execute, start=1):
-        command = ssh(system, command, no_stdin=args.no_stdin)
+    for index, commandline in enumerate(args.execute, start=1):
+        command = ssh(system, commandline, no_stdin=args.no_stdin)
+        LOGGER.debug('Running "%s" on system %i.', commandline, system)
         completed_process = execute(command)
-        job[command] = success = completed_process.returncode == 0
+        job[commandline] = success = completed_process.returncode == 0
 
         if completed_process.returncode == 255:
             raise OfflineError(completed_process)
 
+        LOGGER.debug('Command returned %i.', completed_process.returncode)
+
+        if stdout := completed_process.stdout:
+            LOGGER.info('System %i: %s -> %s', system, commandline, stdout)
+
+        if stderr := completed_process.stderr:
+            LOGGER.warning('System %i: %s -> %s', system, commandline, stderr)
+
         if not success:
             result = False
-            LOGGER.debug('Running "%s" on system %i.', command, system)
             LOGGER.error('Command #%i failed on system %i.', index, system)
-            LOGGER.info('Command returned %i.', completed_process.returncode)
             LOGGER.debug('STDOUT: %s', completed_process.stdout)
             LOGGER.debug('STDERR: %s', completed_process.stderr)
 
