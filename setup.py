b294ca67785b3c907d6751f81bb5e761590ee069
Richard Neumann <r.neumann@homeinfo.de>
2020-10-15 13:14:45 +0200

    Migrated to setuptools.

--- setup.py
+++ setup.py
@@ -1,16 +1,20 @@
 #! /usr/bin/env python3
 
-from distutils.core import setup
+from setuptools import setup
 
 
 setup(
     name='hidsl-scripts',
+    version_format='{tag}',
+    setup_requires=['setuptools-git-version'],
     author='HOMEINFO - Digitale Informationssysteme GmbH',
     author_email='info@homeinfo.de',
     maintainer='Richard Neumann',
     maintainer_email='r.neumann@homeinfo.de',
-    requires=['requests'],
+    install_requires=['requests'],
+    python_requires='>=3.8',
     packages=['hidsl', 'hidsl.query', 'hidsl.rpc', 'hidsl.vpn'],
     scripts=['files/sysquery','files/sysrpc', 'files/sysvpn'],
+    license='GPLv3',
     description='HOMEINFO Digital Signage Linux configurator.'
 )
