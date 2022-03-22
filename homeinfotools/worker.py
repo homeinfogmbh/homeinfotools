e71f60da1619d09b7d15732cdb01a68351120a30
Richard Neumann <r.neumann@homeinfo.de>
2022-03-22 10:38:07 +0100

    Rewrite BaseWorker to store results in dict

--- homeinfotools/worker.py
+++ homeinfotools/worker.py
@@ -1,8 +1,10 @@
 """Multiprocessing worker."""
 
 from argparse import Namespace
+from collections import defaultdict
 from datetime import datetime
 from multiprocessing import Queue
+from queue import Empty
 
 from homeinfotools.exceptions import SSHConnectionError
 from homeinfotools.logging import syslogger
@@ -11,22 +13,33 @@ from homeinfotools.logging import syslogger
 __all__ = ['BaseWorker']
 
 
-class BaseWorker:   # pylint: disable=R0903
+class BaseWorker:
     """Stored args and manager to process systems."""
 
-    __slots__ = ('args', 'systems')
+    __slots__ = ('args', 'systems', 'results')
 
     def __init__(self, args: Namespace, systems: Queue[int]):
         """Sets the command line arguments."""
         self.args = args
         self.systems = systems
+        self.results = defaultdict(dict)
 
-    def __call__(self, system: int) -> tuple[int, dict]:
+    def __call__(self) -> None:
         """Runs the worker on the given system."""
-        result = {'start': (start := datetime.now()).isoformat()}
+        while True:
+            try:
+                system = self.systems.get_nowait()
+            except Empty:
+                return
+
+            self._process_system(system, self.results[system])
+
+    def _process_system(self, system: int, result: dict) -> None:
+        """Processes a single system."""
+        result['start'] = (start := datetime.now()).isoformat()
 
         try:
-            self.run(system, result)
+            result['result'] = self.run(system)
         except SSHConnectionError:
             syslogger(system).error('Could not establish SSH connection.')
             result['success'] = False
@@ -35,8 +48,7 @@ class BaseWorker:   # pylint: disable=R0903
 
         result['end'] = (end := datetime.now()).isoformat()
         result['duration'] = str(end - start)
-        return (system, result)
 
-    def run(self, system: int, result: dict) -> None:
+    def run(self, system: int) -> dict:
         """Runs the respective processes."""
         raise NotImplementedError()
